fc23494f36ee5ec063431249648995bb1edd8c5d
Andre <andre_ranulfo@hotmail.com>
2025-05-02 10:40:51 -0300

    Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import ButtonSimple from './Components/ButtonSimple.jsx'
 import './App.css'
 
 function App() {
@@ -17,6 +18,10 @@ function App() {
     setContact({...contact,email:e.target.value})
   }
 
+  function addContact(){
+    setContactList([...contactList,contact])
+  }
+
 
 
   return (
@@ -24,13 +29,27 @@ function App() {
     <h1>My contact list</h1>
     <hr />
     <div>
+      <div>
+        <lable>Name</lable>
       <input type="text" placeholder="John Doe" onChange={defineName} value={contact.name} />
+      </div>
       <br />
-      <input type='email' placeholder='jdoe@xpto.com' onChange={defineEmail} value={contact.email} />
-      
-   
-      
+      <div>
+        <label>Email</label>
+      <input type='text' placeholder='jdoe@xpto.com' onChange={defineEmail} value={contact.email} />
+      </div>
+      <ButtonSimple value='Add' onClick={addContact} />
+    </div>
+    <div>
+      <ul>
+        {
+          contactList.map((contact,index)=>{
+            return <li key={index}>{contact.name} - {contact.email}</li>
+          })
+        
+        }
 
+      </ul>
 
     </div>
    
---
+++ src/Components/ButtonSimple.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonSimple = ({value='Click'}) => {
+  return (
+    <button>{value}</button>
+  )
+}
+
+export default ButtonSimple
