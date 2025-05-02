8bff95ccbee7e33f0e05518c7ad627036dae921f
Andre <andre_ranulfo@hotmail.com>
2025-05-02 10:29:09 -0300

    Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import React from 'react'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [contact,setContact] = useState({
+    name: "",
+    email: ""
+  })
+  const [contactList,setContactList] = useState([]);
+
+  
+  function defineName(e){
+    setContact({...contact,name:e.target.value})
+  }
+  function defineEmail(e){
+    setContact({...contact,email:e.target.value})
+  }
+
 
 
   return (
     <>
+    <h1>My contact list</h1>
+    <hr />
+    <div>
+      <input type="text" placeholder="John Doe" onChange={defineName} value={contact.name} />
+      <br />
+      <input type='email' placeholder='jdoe@xpto.com' onChange={defineEmail} value={contact.email} />
+      
+   
+      
+
+
+    </div>
    
 
     </>
